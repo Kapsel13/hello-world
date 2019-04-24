6129c74a1fd40b5729c810f2443e67e9bbcc24b6
marcin449 <marcin.kowalski449@gmail.com>
2019-04-24 17:41:55 +0200

    Fixed test

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test
-{public static void main(Strng[] args)
- System.out.println("Hello World")
+{public static void main(String[] args){
+ System.out.print("Hello World");
  int i=1;
  i=i+5;
  int a=0;
